9d9301cd15772fa4b0d432e8178fc15ca355207d
Dorian Roly <dodorol97@gmail.com>
2018-11-12 19:12:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myfirstapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+    }
+}
+
+    fun saveContact (){
+    print("test")
+}
---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myfirstapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+         val intent = Intent(this, DisplayMessageActivity::class.java)
+        startActivity(intent)
+    }
+}
